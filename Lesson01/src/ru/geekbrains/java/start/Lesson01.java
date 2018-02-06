74f7df7022b822fa0cc11d4c5b7fc1a28b44a0d7
amk-nn <alexey.kuznecov.nn@yandex.ru>
2018-02-07 01:28:11 +0300

    update: ignore folders

--- Lesson01/src/ru/geekbrains/java/start/Lesson01.java
+++ Lesson01/src/ru/geekbrains/java/start/Lesson01.java
@@ -5,6 +5,6 @@ public class Lesson01 {
     public static void main(String[] args) {
 
         System.out.println("GeekBrains.ru. Lesson01");
-        System.out.println("I will study Java!");
+        System.out.println("I will study Java!!!");
     }
 }
